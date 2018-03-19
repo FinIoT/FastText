1793366482f94cfa4cfacef82760d35056ca7728
FinIoT <48702236@qq.com>
2018-03-19 22:12:34 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 21:22:41 2018
+
+@author: LIKS
+"""
+print("started...")
+import tensorflow as tf
+import numpy as np
+
+class fastTextB:
+    def __init__(self,):
+        #placeholder for X, and Y; x should be fixed length
+        input_x=tf.placeholder(,shape=[None,self.sentence_le],int32)
